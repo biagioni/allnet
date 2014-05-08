ba16b0209e1afeaa91efc9293385d426559e6ba2
Andreas Brauchli <a.brauchli@elementarea.net>
2014-05-07 19:39:02 -1000

    abc: rework interface so that it supports multiple backends
    Supported interfaces are link-time dependent,
    chosen interface is run-time specific.
    That is, one backend per abc invocation

--- src/abc-iface.h
+++ src/abc-iface.h
@@ -1,10 +1,21 @@
+#ifndef ABC_IFACE_H
+#define ABC_IFACE_H
 /* abc-iface.h: Bradcast abc messages onto a wireless interface */
 
 #include <netpacket/packet.h>  /* struct sockaddr_ll */
 
-int init_iface (const char * interface, int * sock,
-                struct sockaddr_ll * address, struct sockaddr_ll * bc);
-unsigned long long int iface_on_off_ms;
-extern int iface_is_on;
-void iface_on (const char * interface);
-void iface_off (const char * interface);
+/** enum of all compile-time supported abc iface modules */
+typedef enum abc_iface_type {
+  ABC_IFACE_TYPE_WIFI
+} abc_iface_type;
+
+typedef struct abc_iface {
+  abc_iface_type iface_type;
+  int (* init_iface_cb) (const char * interface, int * sock,
+              struct sockaddr_ll * address, struct sockaddr_ll * bc);
+  unsigned long long int iface_on_off_ms;
+  int (* iface_is_enabled_cb) ();
+  int (* iface_set_enabled_cb) (int state);
+} abc_iface;
+
+#endif /* ABC_IFACE_H */
