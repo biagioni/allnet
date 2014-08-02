8feb9cf694edca5af6e2608ccd2f17c5f1ffec94
Andreas Brauchli <a.brauchli@elementarea.net>
2014-08-02 10:44:41 -1000

    First working stream receiver
    Sender is sending packets but they all seem to be late

--- src/voa/main.c
+++ src/voa/main.c
@@ -19,18 +19,31 @@
  * SOFTWARE.
  */
 
+#ifdef ALLNET
 #include <lib/app_util.h> /* connect_to_local */
 #include <lib/packet.h>
 #include <lib/util.h>     /* random_bytes */
+#endif /* ALLNET */
+#ifdef SOCKET
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#endif /* SOCKET */
+#include <gio/gio.h>
 #include <gst/gst.h>
+#include <gst/app/gstappsrc.h>
+#include <gst/app/gstappsink.h>
 #include <stdlib.h>       /* atoi */
 #include <string.h>       /* memcpy */
 
 #define CHUNK_SIZE 1024   /* Amount of bytes we are sending in each buffer */
-#define AUDIO_CAPS "audio/x-opus"
+#define AUDIO_CAPS "application/x-rtp,media=(string)audio,payload=(int)96,clock-rate=(int)48000,encoding-name=(string)X-GST-OPUS-DRAFT-SPITTKA-00"
 
 typedef struct _DecoderData {
   GstElement * voa_source; /* Voice-over-allnet source */
+  GstElement * jitterbuffer;
+  GstElement * rtpdepay;
+  GstElement * decoder;
   GstElement * sink; /* playback device */
   int sourceid;
 } DecoderData;
@@ -40,12 +53,16 @@ typedef struct _EncoderData {
   GstElement * convert;
   GstElement * resample;
   GstElement * encoder;
+  GstElement * rtp;
   GstElement * voa_sink; /* Voice-over-allnet sink */
-
-  guint sourceid;
 } EncoderData;
 
 typedef struct _VOAData {
+#ifdef SOCKET
+  GSocket * gsocket;
+  int socket;
+  struct sockaddr_in dest;
+#endif /* SOCKET */
   GMainLoop * loop;
   GstElement * pipeline;
   gboolean is_encoder;
@@ -55,24 +72,89 @@ typedef struct _VOAData {
   };
 } VOAData;
 
-/* voa_sink has received a buffer */
-static void new_buffer (GstElement * sink, VOAData * data) {
-  GstBuffer * buffer;
+static void dec_handle_data_free_buffer (gpointer data) {
+  g_free (data);
+}
 
-  /* Retrieve the buffer */
-  g_signal_emit_by_name (sink, "pull-buffer", &buffer);
-  if (buffer) {
-    /* TODO: send buffer over allnet */
+static gboolean dec_handle_data (GIOChannel * source, GIOCondition condition, gpointer data) {
+  gchar * buffer = g_new (gchar, 1024);
+  gsize length;
+  GError * err = NULL;
+  GstFlowReturn ret;
+
+  GIOStatus iostat;
+  iostat = g_io_channel_read_chars (source, buffer, 1024, &length, &err);
+  if (iostat != G_IO_STATUS_NORMAL)
+    g_printerr ("couldn't read packets %s\n", iostat == G_IO_STATUS_AGAIN ? "interface busy" : "unknown issue");
+  if (err)
+    g_printerr ("Error in dec_handle_data: %s", err->message);
+  if (iostat != G_IO_STATUS_NORMAL)
+    return TRUE;
+  g_print ("read %lu bytes\n", length);
+  if (length == 0)
+    return TRUE;
+
+  GstBuffer * gstbuf = gst_buffer_new_wrapped (buffer, length);
+
+  /* Push the buffer into the appsrc */
+  VOAData * d = (VOAData *)data;
+  g_signal_emit_by_name (d->dec.voa_source, "push-buffer", gstbuf, &ret);
+  GValue val = G_VALUE_INIT;
+  g_value_init (&val, G_TYPE_INT);
+  g_object_get_property (G_OBJECT (d->dec.jitterbuffer), "percent", &val);
+  gint percent = g_value_get_int (&val);
+  g_print ("Jitterbuffer %d\n", percent);
+  gst_buffer_unref (gstbuf);
+  if (ret != GST_FLOW_OK) {
+    g_printerr ("error inserting packets into gst pipeline\n");
+    return FALSE; /* We got some error, stop sending data */
+  }
+  GstState st, pst;
+  gst_element_get_state (d->pipeline, &st, &pst, 0);
+  g_print ("state: %d, pending: %d\n", st, pst);
+  if (st != GST_STATE_PLAYING && pst != GST_STATE_PLAYING && pst != GST_STATE_PLAYING) {
+    gst_element_set_state (d->pipeline, GST_STATE_PLAYING);
+  }
+
+  return TRUE;
+}
+
+static void enc_eos (GstAppSink * sink, gpointer data) {
+  g_print ("EOS received\n");
+}
+
+/* voa_sink has received a sample */
+static GstFlowReturn enc_new_sample (GstAppSink * sink, gpointer p) {
+  GstSample * sample;
+
+  VOAData * data = (VOAData *)p;
+
+  /* Retrieve the sample */
+  g_signal_emit_by_name (sink, "pull-sample", &sample);
+  if (sample) {
+    GstBuffer * buffer = gst_sample_get_buffer (sample);
+    gsize bufsiz = gst_buffer_get_size (buffer);
+#ifdef ALLNET
     /* TODO: create message */
     // int pak_size;
-    // struct allnet_header * pak = create_packet (buf_siz, ALLNET_TYPE_DATA, 10 /*hops*/, NULL/*src addr*/, 0 /*src bits*/, NULL, 0 /*dst*/, NULL /*ack*/);
+    // struct allnet_header * pak = create_packet (buf_siz, ALLNET_TYPE_DATA, 3 /*max hops*/, NULL/*src addr*/, 0 /*src bits*/, NULL, 0 /*dst*/, NULL /*ack*/);
     // pak->transport = ALLNET_TRANSPORT_STREAM;
     // char * sid = new_msg_id ();
     // pak->stream_id = sid;
     // send_pipe_message (sock, msg, pak_size, ALLNET_PRIORITY_HIGH);
+#endif /* ALLNET */
+#ifdef SOCKET
+    if (sendto (data->socket, buffer, bufsiz, 0, (const struct sockaddr *)&data->dest, sizeof (data->dest)) == -1)
+      g_printerr ("error sending\n");
+    g_print ("size: %lu\n", bufsiz);
+#endif /* SOCKET */
+    g_print (".");
 
-    gst_buffer_unref (buffer);
-  }
+    gst_sample_unref (sample);
+  } else
+    g_print ("NULL sample\n");
+
+  return GST_FLOW_OK;
 }
 
 static void cb_message (GstBus * bus, GstMessage * msg, VOAData * data) {
@@ -83,7 +165,7 @@ static void cb_message (GstBus * bus, GstMessage * msg, VOAData * data) {
       gchar * debug;
 
       gst_message_parse_error (msg, &err, &debug);
-      g_print ("Error: %s\n", err->message);
+      g_printerr ("Error: %s\n", err->message);
       g_error_free (err);
       g_free (debug);
 
@@ -93,15 +175,13 @@ static void cb_message (GstBus * bus, GstMessage * msg, VOAData * data) {
     }
     case GST_MESSAGE_EOS:
       /* end-of-stream */
+      g_print ("EOS Msg\n");
       gst_element_set_state (data->pipeline, GST_STATE_READY);
       g_main_loop_quit (data->loop);
       break;
     case GST_MESSAGE_BUFFERING: {
+      /* CHECK: not sure we really need this, since live streams don't buffer */
       gint percent = 0;
-
-      /* If the stream is live, we do not care about buffering. */
-      //if (data->is_live) break; // live streams don't pause
-
       gst_message_parse_buffering (msg, &percent);
       g_print ("Buffering (%3d%%)\r", percent);
       /* Wait until buffering is complete before start/resume playing */
@@ -113,66 +193,13 @@ static void cb_message (GstBus * bus, GstMessage * msg, VOAData * data) {
     }
     case GST_MESSAGE_CLOCK_LOST:
       /* Get a new clock */
+      g_print ("lost clock\n");
       gst_element_set_state (data->pipeline, GST_STATE_PAUSED);
       gst_element_set_state (data->pipeline, GST_STATE_PLAYING);
       break;
     default:
       /* Unhandled message */
       break;
-  }
-}
-
-/* This method is called by the idle GSource in the mainloop, to feed CHUNK_SIZE bytes into voa_src.
- * The idle handler is added to the mainloop when appsrc requests us to start sending data (need-data signal)
- * and is removed when voa_src has enough data (enough-data signal).
- */
-static gboolean push_data (VOAData *data) {
-  GstBuffer *buffer;
-  GstFlowReturn ret;
-  int i;
-  gint16 *raw;
-  gint num_samples = CHUNK_SIZE / 2; /* Because each sample is 16 bits */
-  gfloat freq;
-
-  /* Create a new empty buffer for voa data */
-  buffer = gst_buffer_new_and_alloc (CHUNK_SIZE);
-
-  /* .. */
-
-  /* Set its timestamp and duration */
-  // GST_BUFFER_TIMESTAMP (buffer) = gst_util_uint64_scale (data->num_samples, GST_SECOND, SAMPLE_RATE);
-  // GST_BUFFER_DURATION (buffer) = gst_util_uint64_scale (CHUNK_SIZE, GST_SECOND, SAMPLE_RATE);
-
-  /* Push the buffer into the appsrc */
-  g_signal_emit_by_name (data->dec.voa_source, "push-buffer", buffer, &ret);
-
-  /* Free the buffer now that we are done with it */
-  gst_buffer_unref (buffer);
-
-  if (ret != GST_FLOW_OK) {
-    /* We got some error, stop sending data */
-    return FALSE;
-  }
-  return TRUE;
-}
-
-/* This signal callback triggers when appsrc needs data. Here, we add an idle handler
- * to the mainloop to start pushing data into the appsrc */
-static void start_feed (GstElement *source, guint size, VOAData *data) {
-  if (data->dec.sourceid == 0) {
-    g_print ("Start feeding\n");
-    data->dec.sourceid = g_idle_add ((GSourceFunc) push_data, data);
-  }
-}
-
-
-/* This callback triggers when appsrc has enough data and we can stop sending.
-* We remove the idle handler from the mainloop */
-static void stop_feed (GstElement *source, VOAData *data) {
-  if (data->dec.sourceid != 0) {
-    g_print ("Stop feeding\n");
-    g_source_remove (data->dec.sourceid);
-    data->dec.sourceid = 0;
   }
 }
 
@@ -187,93 +214,165 @@ static int init_audio (gboolean is_encoder)
   int argc = 0;
   char * argv[] = { "" };
   gst_init (&argc, (char ***)&argv);
-
-  /* Create the elements */
-  if (is_encoder) {
-    data.enc.source = gst_element_factory_make ("audiotestsrc", "source"); /* autoaudiosource */
-    data.enc.convert = gst_element_factory_make ("audioconvert", "convert");
-    data.enc.resample = gst_element_factory_make ("audioresample", "resample");
-    data.enc.encoder = gst_element_factory_make ("opusenc", "encode");
-    data.enc.voa_sink = gst_element_factory_make ("appsink", "voa_sink");
-  } else {
-    data.dec.voa_source = gst_element_factory_make ("appsource", "voa_source");
-    data.dec.sink = gst_element_factory_make ("autoaudiosink", "sink");
-  }
-
   GstCaps * appcaps = gst_caps_from_string (AUDIO_CAPS);
-  if (is_encoder) {
-    /* Configure encoder appsink */
-    g_object_set (data.enc.voa_sink, "emit-signals", TRUE, "caps", appcaps, NULL);
-    g_signal_connect (data.enc.voa_sink, "new-buffer", G_CALLBACK (new_buffer), &data);
 
-    /* Modify the source's properties */
-    g_object_set (data.enc.encoder, "bandwidth", 1101,
-                                    "bitrate", 4000,
-                                    "cbr", FALSE,
-                                    NULL);
-
-  } else {
-    /* Configure decoder source */
-    g_object_set (data.dec.voa_source, "caps", appcaps, NULL);
-    g_signal_connect (data.dec.voa_source, "need-data", G_CALLBACK (start_feed), &data);
-    g_signal_connect (data.dec.voa_source, "enough-data", G_CALLBACK (stop_feed), &data);
-  }
-  gst_caps_unref (appcaps);
-
-  /* Create the empty pipeline and build it */
+  /* Create the empty pipeline */
   data.pipeline = gst_pipeline_new ("pipeline");
   if (!data.pipeline) {
     g_printerr ("Couldn't create pipeline.\n");
     return -1;
   }
 
+  /* Create the elements */
   if (is_encoder) {
-    if (!data.enc.source || !data.enc.convert || !data.enc.voa_sink) {
+    //data.enc.source = gst_element_factory_make ("audiotestsrc", "source");
+    data.enc.source = gst_element_factory_make ("autoaudiosrc", "source");
+    data.enc.convert = gst_element_factory_make ("audioconvert", "convert");
+    data.enc.resample = gst_element_factory_make ("audioresample", "resample");
+    data.enc.encoder = gst_element_factory_make ("opusenc", "encoder");
+    data.enc.rtp = gst_element_factory_make ("rtpopuspay", "rtp");
+    data.enc.voa_sink = gst_element_factory_make ("appsink", "voa_sink");
+
+    if (!data.enc.source || !data.enc.convert || !data.enc.resample ||
+        !data.enc.encoder || !data.enc.rtp || !data.enc.voa_sink) {
       g_printerr ("Not all elements could be created.\n");
       return -1;
     }
-    gst_bin_add_many (GST_BIN (data.pipeline), data.enc.source, data.enc.convert, data.enc.voa_sink, NULL);
-    if (gst_element_link_many (data.enc.source, data.enc.convert, data.enc.voa_sink, NULL) != TRUE) {
+    /* Configure encoder appsink */
+    g_object_set (data.enc.voa_sink, "emit-signals", TRUE, /*"caps", appcaps,*/ NULL);
+    GstAppSinkCallbacks cbs = {
+      .eos = enc_eos,
+      .new_preroll = NULL,
+      .new_sample = enc_new_sample
+    };
+    gst_app_sink_set_callbacks (GST_APP_SINK (data.enc.voa_sink), &cbs, &data, NULL);
+
+    /* Modify the source's properties */
+    g_object_set (data.enc.encoder, "bandwidth", 1101, /* narrowband */
+                                    "bitrate", 4000,
+                                    "cbr", FALSE, /* constant bit rate */
+                                    NULL);
+
+    gst_bin_add_many (GST_BIN (data.pipeline), data.enc.source,
+            data.enc.convert, data.enc.resample, data.enc.encoder,
+            data.enc.rtp, data.enc.voa_sink, NULL);
+    if (! gst_element_link_many (data.enc.source,
+            data.enc.convert, data.enc.resample, data.enc.encoder,
+            data.enc.rtp, data.enc.voa_sink, NULL)) {
       g_printerr ("Elements could not be linked.\n");
       gst_object_unref (data.pipeline);
       return -1;
     }
 
   } else {
-    if (!data.dec.voa_source || !data.dec.sink) {
+    /* decoder */
+    data.dec.sourceid = 0;
+    data.dec.voa_source = gst_element_factory_make ("appsrc", "voa_source");
+    data.dec.jitterbuffer = gst_element_factory_make ("rtpjitterbuffer", "jitterbuffer");
+    data.dec.rtpdepay = gst_element_factory_make ("rtpopusdepay", "rtpdepay");
+    data.dec.decoder = gst_element_factory_make ("opusdec", "decoder");
+    data.dec.sink = gst_element_factory_make ("autoaudiosink", "sink");
+    if (!data.dec.voa_source || !data.dec.jitterbuffer || !data.dec.rtpdepay ||
+        !data.dec.decoder || !data.dec.sink) {
       g_printerr ("Not all elements could be created.\n");
       return -1;
     }
-    gst_bin_add_many (GST_BIN (data.pipeline), data.dec.voa_source, data.dec.sink, NULL);
-    if (gst_element_link_many (data.dec.voa_source, data.dec.sink, NULL) != TRUE) {
+    /* Configure decoder source */
+    g_object_set (data.dec.voa_source,
+      "stream-type", GST_APP_STREAM_TYPE_STREAM,
+      "format", GST_FORMAT_TIME /*_BYTES?*/,
+      "caps", appcaps,
+      NULL);
+    g_object_set (data.dec.jitterbuffer, "latency", 100, "do-lost", TRUE, NULL); /* opus: 20ms of data per packet */
+    g_object_set (data.dec.decoder, "plc", TRUE, NULL); /* packet loss concealment */
+
+    gst_bin_add_many (GST_BIN (data.pipeline), data.dec.voa_source,
+            data.dec.jitterbuffer, data.dec.rtpdepay, data.dec.decoder,
+            data.dec.sink, NULL);
+    if (! gst_element_link_many (data.dec.voa_source,
+            data.dec.jitterbuffer, data.dec.rtpdepay, data.dec.decoder,
+            data.dec.sink, NULL)) {
       g_printerr ("Elements could not be linked.\n");
       gst_object_unref (data.pipeline);
       return -1;
     }
   }
-
-  /* Start playing */
-  ret = gst_element_set_state (data.pipeline, GST_STATE_PLAYING);
-  if (ret == GST_STATE_CHANGE_FAILURE) {
-    g_printerr ("Unable to set the pipeline to the playing state.\n");
-    gst_object_unref (data.pipeline);
-    return -1;
-  }
-
-  data.loop = g_main_loop_new (NULL, FALSE);
+  gst_caps_unref (appcaps);
 
   /* Wait until error or EOS */
   bus = gst_element_get_bus (data.pipeline);
   g_signal_connect (bus, "message", G_CALLBACK (cb_message), &data);
 
+#ifdef SOCKET
+  guint source;
+  if (is_encoder) {
+    data.socket = socket (AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    struct sockaddr_in sa;
+    memset (&sa, 0, sizeof (struct sockaddr_in));
+    sa.sin_family = AF_INET;
+    sa.sin_addr.s_addr = inet_addr ("127.0.0.1");
+    sa.sin_port = htons (12534);
+    memset (&data.dest, 0, sizeof (struct sockaddr_in));
+    data.dest.sin_family = AF_INET;
+    data.dest.sin_addr.s_addr = inet_addr ("127.0.0.1");
+    data.dest.sin_port = htons (12535);
+    if (bind (data.socket, (struct sockaddr *)&sa, sizeof (struct sockaddr_in)) == -1)
+      g_printerr ("couldn't bind socket\n");
+
+  } else {
+
+    GError * err = NULL;
+    data.gsocket = g_socket_new (G_SOCKET_FAMILY_IPV4,
+                                  G_SOCKET_TYPE_DATAGRAM,
+                                  G_SOCKET_PROTOCOL_UDP,
+                                  &err);
+    if (err != NULL) {
+      g_printerr("%s\n", err->message);
+      return -1;
+    }
+    g_socket_bind (data.gsocket,
+      G_SOCKET_ADDRESS (g_inet_socket_address_new
+        (g_inet_address_new_any (G_SOCKET_FAMILY_IPV4), 12535)),
+      FALSE, &err);
+    if (err != NULL) {
+      g_printerr("%s\n", err->message);
+      return -1;
+    }
+    int fd = g_socket_get_fd (data.gsocket);
+    GIOChannel * channel = g_io_channel_unix_new (fd);
+    g_io_channel_set_encoding (channel, NULL, NULL);
+    g_io_channel_set_buffered (channel, FALSE);
+    source = g_io_add_watch (channel, G_IO_IN,
+                              (GIOFunc) dec_handle_data, &data);
+    g_print ("Listening for traffic on port %d..\n", 12535);
+    g_io_channel_unref (channel);
+  }
+#endif /* SOCKET */
+
   /* Start playing the pipeline */
-  gst_element_set_state (data.pipeline, GST_STATE_PLAYING);
+  ret = gst_element_set_state (data.pipeline, GST_STATE_PAUSED);
+  if (ret == GST_STATE_CHANGE_FAILURE) {
+    g_printerr ("Unable to change pipeline state.\n");
+    gst_object_unref (data.pipeline);
+    return -1;
+  }
+
+  if (is_encoder) {
+    gst_element_set_state (data.pipeline, GST_STATE_PLAYING);
+    //while (TRUE) enc_new_sample (data.enc.voa_sink, &data);
+  }
 
   /* Create a GLib Main Loop and set it to run */
   data.loop = g_main_loop_new (NULL, FALSE);
   g_main_loop_run (data.loop);
 
   /* Free resources */
+#ifdef SOCKET
+  if (!is_encoder) {
+    g_source_remove (source);
+    g_object_unref (data.gsocket);
+  }
+#endif /* SOCKET */
   g_main_loop_unref (data.loop);
   gst_object_unref (bus);
   gst_element_set_state (data.pipeline, GST_STATE_NULL);
@@ -283,6 +382,7 @@ static int init_audio (gboolean is_encoder)
 
 int main (int argc, char ** argv)
 {
+#ifdef ALLNET
   int sock = connect_to_local (argv [0], argv [0]);
   if (sock < 0)
     return 1;
@@ -305,6 +405,7 @@ int main (int argc, char ** argv)
     dest_addr_bits = argc > 1 ? atoi (argv[2]) : strnlen (argv[0], ADDRESS_SIZE);
     memcpy (dest_address, argv[1], dest_addr_bits);
   }
+#endif /* ALLNET */
 
   gboolean is_encoder = (strcmp (argv [0], "./voas") == 0);
   g_print ("is_encoder: %d\n", is_encoder);
