33c4031e39b9afe67bf9fd96c6f0f62377e52249
Andreas Brauchli <a.brauchli@elementarea.net>
2014-08-14 12:34:46 -1000

    abc: Remove unneeded header include

--- src/abc/abc.c
+++ src/abc/abc.c
@@ -45,7 +45,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/socket.h>
 #include <sys/time.h>
 #include <unistd.h>           /* usleep */
 #include <netpacket/packet.h> /* sockaddr_ll */
