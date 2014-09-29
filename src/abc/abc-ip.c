4c60b76d6f2e9aa6631657443ae7d7f572a94dc9
Andreas Brauchli <a.brauchli@elementarea.net>
2014-09-28 19:41:24 -1000

    abc-iface: merge common functions into abc-iface.c
    This commit depends on the next for a compiling binary

--- src/abc/abc-ip.c
+++ src/abc/abc-ip.c
@@ -34,41 +34,6 @@ abc_iface abc_iface_ip = {
   .iface_cleanup_cb = abc_ip_cleanup
 };
 
-#ifndef __APPLE__  /* not sure what replaces the sll addresses for apple */
-static void default_broadcast_address (struct sockaddr_ll * bc)
-{
-  bc->sll_family = AF_PACKET;
-  bc->sll_protocol = ALLNET_WIFI_PROTOCOL;
-  bc->sll_hatype = 1;   /* used? */
-  bc->sll_pkttype = 0;  /* not used */
-  bc->sll_halen = 6;
-  bc->sll_addr [0] = 0xff;
-  bc->sll_addr [1] = 0xff;
-  bc->sll_addr [2] = 0xff;
-  bc->sll_addr [3] = 0xff;
-  bc->sll_addr [4] = 0xff;
-  bc->sll_addr [5] = 0xff;
-}
-
-static void print_sll_addr (struct sockaddr_ll * a, char * desc)
-{
-  if (desc != NULL)
-    printf ("%s: ", desc);
-  if (a->sll_family != AF_PACKET) {
-    printf ("unknown address family %d\n", a->sll_family);
-    return;
-  }
-  printf ("proto %d, ha %d pkt %d halen %d ", a->sll_protocol, a->sll_hatype,
-          a->sll_pkttype, a->sll_halen);
-  int i;
-  for (i = 0; i < a->sll_halen; i++) {
-    if (i > 0) printf (":");
-    printf ("%02x", a->sll_addr [0]);
-  }
-  if (desc != NULL)
-    printf ("\n");
-}
-#endif /* __APPLE__ */
 
 static int abc_ip_is_enabled ()
 {
--- src/abc/abc-wifi.c
+++ src/abc/abc-wifi.c
@@ -62,43 +62,6 @@ static abc_wifi_config_iface * wifi_config_types[] = {
 };
 static abc_wifi_config_iface * wifi_config_iface = NULL;
 
-#ifndef __APPLE__  /* not sure what replaces the sll addresses for apple */
-static void default_broadcast_address (struct sockaddr_ll * bc)
-{
-  bc->sll_family = AF_PACKET;
-  bc->sll_protocol = ALLNET_WIFI_PROTOCOL;
-  bc->sll_hatype = 1;   /* used? */
-  bc->sll_pkttype = 0;  /* not used */
-  bc->sll_halen = 6;
-  bc->sll_addr [0] = 0xff;
-  bc->sll_addr [1] = 0xff;
-  bc->sll_addr [2] = 0xff;
-  bc->sll_addr [3] = 0xff;
-  bc->sll_addr [4] = 0xff;
-  bc->sll_addr [5] = 0xff;
-  printf ("set default broadcast address\n");
-}
-
-static void print_sll_addr (struct sockaddr_ll * a, char * desc)
-{
-  if (desc != NULL)
-    printf ("%s: ", desc);
-  if (a->sll_family != AF_PACKET) {
-    printf ("unknown address family %d\n", a->sll_family);
-    return;
-  }
-  printf ("proto %d, ha %d pkt %d halen %d ", a->sll_protocol, a->sll_hatype,
-          a->sll_pkttype, a->sll_halen);
-  int i;
-  for (i = 0; i < a->sll_halen; i++) {
-    if (i > 0) printf (":");
-    printf ("%02x", a->sll_addr [0]);
-  }
-  if (desc != NULL)
-    printf ("\n");
-}
-#endif /* __APPLE__ */
-
 static int abc_wifi_is_enabled ()
 {
   return wifi_config_iface->iface_is_enabled_cb () &&
