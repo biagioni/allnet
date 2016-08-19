b47910c7a8176210084c9a9163dff1349f2124e5
edo <esb@hawaii.edu>
2016-08-18 16:25:24 -1000

    MessageBubble.java: Henry's changes to support a copy menu on bubbles

--- src/xchat_ui/utils/MessageBubble.java
+++ src/xchat_ui/utils/MessageBubble.java
@@ -1,9 +1,14 @@
+
 package utils;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.BoxLayout;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
+import javax.swing.JPopupMenu;
 import javax.swing.JTextPane;
 
 /**
@@ -14,10 +19,13 @@ import javax.swing.JTextPane;
  * @author henry
  * @param <MESSAGE>
  */
-public class MessageBubble<MESSAGE> extends JPanel {
+public class MessageBubble<MESSAGE> extends JPanel implements ActionListener {
+
+    private static final String COPY = "Copy";
+    private static final String COPY_ALL = "Copy All";
 
     // keep the message pane so we can change background later
-    private JTextPane area;
+    private JTextPane textPane;
     // keep a reference to the message that the Bubble renders, if desired
     private MESSAGE message;
 
@@ -29,11 +37,11 @@ public class MessageBubble<MESSAGE> extends JPanel {
         super();
         this.message = message;
         setBackground(color);
-        area = new JTextPane();
-        area.setContentType("text/html");
-        area.setEditable(false);
-        area.setBackground(color);
-        area.setLayout(new BoxLayout(area, BoxLayout.Y_AXIS));
+        textPane = new JTextPane();
+        textPane.setContentType("text/html");
+        textPane.setEditable(false);
+        textPane.setBackground(color);
+        textPane.setLayout(new BoxLayout(textPane, BoxLayout.Y_AXIS));
         String htmlPrefix;
         if (leftJustified) {
             htmlPrefix = "<STYLE type=\"text/css\"> BODY {text-align: left} </STYLE> <BODY>";
@@ -49,23 +57,50 @@ public class MessageBubble<MESSAGE> extends JPanel {
             }
         }
         sb.append("</BODY>");
-        area.setText(sb.toString());
-        Dimension size = area.getPreferredScrollableViewportSize();
-        area.setPreferredSize(size);
-        area.setMaximumSize(size);
+        textPane.setText(sb.toString());
+        Dimension size = textPane.getPreferredScrollableViewportSize();
+        textPane.setPreferredSize(size);
+        textPane.setMaximumSize(size);
         //
         setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
-        add(area);
+        add(textPane);
         //setBorder(new RoundedBorder(borderColor, borderWidth, borderRadius, borderInset));
+
+        // make a context menu
+        JPopupMenu popup = new JPopupMenu();
+        JMenuItem item = new JMenuItem(COPY);
+        item.addActionListener(this);
+        popup.add(item);
+        item = new JMenuItem(COPY_ALL);
+        item.addActionListener(this);
+        popup.add(item);
+        textPane.setComponentPopupMenu(popup);
+
     }
 
     public void setBubbleBackground(Color bg) {
         super.setBackground(bg);
-        area.setBackground(bg);
+        textPane.setBackground(bg);
     }
 
     public MESSAGE getMessage() {
         return (message);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String cmd = e.getActionCommand();
+        switch (cmd) {
+            case COPY:
+                this.textPane.copy();
+                break;
+            case COPY_ALL:
+                this.textPane.selectAll();
+                this.textPane.copy();
+                break;
+            default:
+                throw new RuntimeException("bad menu cmd");
+        }
     }
 
 }
