f54a6c28f3eee177403ec6bd7ec1f63bed965813
edo <esb@hawaii.edu>
2017-03-23 15:46:25 -1000

    trace_util.c: report min/mean/max in seconds, not ms

--- src/lib/trace_util.c
+++ src/lib/trace_util.c
@@ -307,14 +307,14 @@ static void print_summary_file (int signal, int null_term, int fd_out,
                        "sent %d %s, received %d, ",
                        sent_count, ps, received_count);
       off += snprintf (buf + off, sizeof (buf) - off,
-                       "rtt min/mean/max is %" PRId64 ".%03d/", min_rtt / 1000,
-                       (int) (min_rtt % 1000));
+                       "rtt min/mean/max is %" PRId64 ".%06d/",
+                       min_rtt / 1000000, (int) (min_rtt % 1000000));
       off += snprintf (buf + off, sizeof (buf) - off,
-                       "%" PRId64 ".%03d/", mean_rtt / 1000,
-                       (int) (mean_rtt % 1000));
+                       "%" PRId64 ".%06d/", mean_rtt / 1000000,
+                       (int) (mean_rtt % 1000000));
       off += snprintf (buf + off, sizeof (buf) - off,
-                       "%" PRId64 ".%03d\n", max_rtt / 1000,
-                       (int) (max_rtt % 1000));
+                       "%" PRId64 ".%06ds\n", max_rtt / 1000000,
+                       (int) (max_rtt % 1000000));
     } else {  /* received_count is 0 */
       off += snprintf (buf + off, sizeof (buf) - off, "sent %d %s, received 0\n", sent_count, ps);
     }
