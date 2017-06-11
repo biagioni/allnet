e554fb8b7ca4e53729612956d45e421e06a41074
edo <esb@hawaii.edu>
2017-06-11 05:02:52 -1000

    autogen.sh: support systems that have /usr/local/share/aclocal instead of /usr/share/aclocal

--- autogen.sh
+++ autogen.sh
@@ -5,4 +5,10 @@ fi;
 if [ -z "$AUTOMAKE" ]; then
 	AUTOMAKE="automake";
 fi;
-ACLOCAL="$ACLOCAL -I /usr/share/aclocal/" AUTOMAKE="$AUTOMAKE --foreign" autoreconf -vfi
+if [ -e /usr/local/share/aclocal ]; then
+	ACPREFIX="/usr/local/share/aclocal"
+fi;
+if [ -e /usr/share/aclocal ]; then
+	ACPREFIX="/usr/share/aclocal"
+fi;
+ACLOCAL="$ACLOCAL -I $ACPREFIX" AUTOMAKE="$AUTOMAKE --foreign" autoreconf -vfi
