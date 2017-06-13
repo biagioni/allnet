89665b653dad2f15ea5ee33c81145bc39799cd76
edo <esb@hawaii.edu>
2017-06-13 01:40:30 -1000

    AllNetContacts.java: many changes to support incomplete key exchanges.
    New public methods are:
        public static void deleteEntireContact(String contact) {
        public static void unhideContact(String contact) {
        public static void completeExchange(String contact) {
        public static keyExchangeComplete contactComplete(String contactN
        public static String contactExchangeFile(String contactName) {
        public static int exchangeHopCount(String contactName) {
        public static String firstSecret(String contactName) {
        public static String secondSecret(String contactName) {

--- src/xchat_ui/allnetui/AllNetContacts.java
+++ src/xchat_ui/allnetui/AllNetContacts.java
@@ -4,6 +4,11 @@ package allnetui;
 
 public class AllNetContacts {
 
+    /* in the second case, we have the key, but the peer may not */
+    public enum keyExchangeComplete { INCOMPLETE_NO_KEY,
+                                      INCOMPLETE_WITH_KEY,
+                                      COMPLETE};
+
     private static java.nio.file.Path getContactsDir(String dir) {
         String home = System.getenv ("HOME");
         String profile = System.getenv ("USERPROFILE");
@@ -90,8 +95,8 @@ public class AllNetContacts {
                 }
             }
         } catch (java.io.IOException e) {
-            System.out.println ("IO exception " + e +
-                                " iterating over directory " + path +
+            System.out.println ("AllNetContacts getBroadcast: IO exception " +
+                                e + " iterating over directory " + path +
                                 ", aborting");
             return noResult;
         }
@@ -125,8 +130,8 @@ public class AllNetContacts {
                 }
             }
         } catch (java.io.IOException e) {
-            System.out.println ("IO exception " + e +
-                                " iterating over directory " + cpath +
+            System.out.println ("AllNetContacts chatTimeFile: IO exception " +
+                                e + " iterating over directory " + cpath +
                                 ", aborting");
         }
         return null;
@@ -206,9 +211,96 @@ public class AllNetContacts {
                 }
             }
         } catch (java.io.IOException e) {
-            System.out.println ("IO exception " + e +
+            System.out.println ("AllNetContacts messages: IO exception " + e +
                                 " iterating over directory " + cpath +
                                 ", aborting");
+        }
+    }
+
+    // code largely copied from:
+    // http://www.concretepage.com/java/jdk7/traverse-directory-structure-using-files-walkfiletree-java-nio2
+    private static void deleteSubtree(final String contact, final String top,
+                                      final java.nio.file.Path path,
+                                      Boolean reportErrors) {
+        try {
+            java.nio.file.Files.walkFileTree (path,
+                                              new java.nio.file.SimpleFileVisitor<java.nio.file.Path>() {
+                @Override
+                public java.nio.file.FileVisitResult
+                    postVisitDirectory(java.nio.file.Path dir,
+                                       java.io.IOException e)
+                    throws java.io.IOException {
+                    if (e == null) {
+                        System.out.println("deleting dir " + dir);
+                        java.nio.file.Files.delete(dir);
+                        return java.nio.file.FileVisitResult.CONTINUE;
+                    } else {
+                        System.out.println("Exception while iterating directory.");
+                        throw e;
+                    }
+                }
+                @Override
+                public java.nio.file.FileVisitResult
+                    visitFile(java.nio.file.Path file,
+                              java.nio.file.attribute.BasicFileAttributes attrs)
+                    throws java.io.IOException {
+                    System.out.println("deleting " + file);
+                    java.nio.file.Files.delete(file);
+                    return java.nio.file.FileVisitResult.CONTINUE;
+                }
+            });
+            // System.out.println("Directory Structure Deleted.");
+        } catch (java.io.IOException e) { // ignore
+            if (reportErrors)
+                System.out.println("exception " + e + " deleting subtree");
+        }
+    }
+
+    private static java.nio.file.Path findCpath(String contact) {
+        java.nio.file.Path top_path = getContactsDir("contacts");
+        try (java.nio.file.DirectoryStream<java.nio.file.Path> stream =
+                 java.nio.file.Files.newDirectoryStream (top_path)) {
+            for (java.nio.file.Path p: stream) {
+                if (java.nio.file.Files.isDirectory(p)) {
+                    java.nio.file.Path name =
+                        java.nio.file.FileSystems.
+                            getDefault().getPath(p + "/name");
+                    java.nio.charset.Charset charset =
+                        java.nio.charset.Charset.forName("UTF-8");
+                    java.util.List<String> names =
+                        java.nio.file.Files.readAllLines(name, charset);
+                    for (String s: names) {
+                        if (contact.equals(s)) {
+                            return p;
+                        }
+                    }
+                }
+            }
+        } catch (java.io.IOException e) {
+            System.out.println ("AllNetContacts messages: IO exception " + e +
+                                " finding path for contact " + contact +
+                                ", top path is " + top_path);
+        }
+        return null;
+    }
+
+    public static void deleteEntireContact(String contact) {
+        java.nio.file.Path path = findCpath(contact);
+        if (path == null) {
+            return;
+        }
+        deleteSubtree (contact, "contacts", path, true);
+        String xdir = path.toString().replace("contacts", "xchat");
+        try {
+            java.nio.file.Path xpath =
+                java.nio.file.FileSystems.getDefault().getPath(xdir);
+            if (xpath != null) {
+                deleteSubtree (contact, "xchat", xpath, false);
+            }
+        } catch (java.lang.Exception e) { // silent, after done debugging
+            System.out.println ("AllNetContacts delete xchat exception " + e +
+                                ", contact " + contact +
+                                ", top path is " + path);
         }
     }
 
@@ -217,8 +309,12 @@ public class AllNetContacts {
         // file system, and we detect that anyway.
     }
 
-    // delete the exchange file, if any
-    public static void completeExchange(String contact) {
+    private enum fileActions { EXISTS,    // return non-null iff exists
+                               GET,       // returns file contents if exists
+                               DELETE };  // delete file if exists
+    private static String fileAction(String contact, String fname,
+                                     fileActions action) {
+        String result = null;
         java.nio.file.Path cpath = getContactsDir("contacts");
         try (java.nio.file.DirectoryStream<java.nio.file.Path> stream =
                  java.nio.file.Files.newDirectoryStream (cpath)) {
@@ -233,20 +329,117 @@ public class AllNetContacts {
                         java.nio.file.Files.readAllLines(name, charset);
                     for (String s: names) {
                         if (contact.equals(s)) {
-                            java.nio.file.Path exchange =
+                            java.nio.file.Path theFile =
                                 java.nio.file.FileSystems.
-                                    getDefault().getPath(p + "/exchange");
-                            java.nio.file.Files.deleteIfExists (exchange);
-                            return;
+                                    getDefault().getPath(p + "/" + fname);
+                            switch (action) {
+                            case EXISTS:
+                                if (java.nio.file.Files.exists (theFile)) {
+                                    return "exists";
+                                }
+                                break;
+                            case GET:
+                                try {
+                                    return new String (
+                                        java.nio.file.Files.readAllBytes (theFile));
+                                } catch (java.nio.file.NoSuchFileException e) {
+                                    return null;  // fail gracefully
+                                } 
+                            case DELETE:
+                                java.nio.file.Files.deleteIfExists (theFile);
+                                break;
+                            }
                         }
                     }
                 }
             }
         } catch (java.io.IOException e) {
-            System.out.println ("IO exception " + e +
+            System.out.println ("AllNetContacts " + action + " (" +
+                                fname + "): IO exception " + e +
                                 " iterating over directory " + cpath +
                                 ", aborting");
         }
+        return result;
+    }
+
+    private static void deleteFileIfAny (String contact, String fname) {
+        fileAction (contact, fname, fileActions.DELETE);
+    }
+
+    private static Boolean fileExists (String contact, String fname) {
+        return (fileAction (contact, fname, fileActions.EXISTS) != null);
+    }
+
+    private static String fileContents (String contact, String fname) {
+        return fileAction (contact, fname, fileActions.GET);
+    }
+
+    // delete the hidden file, if any
+    public static void unhideContact(String contact) {
+        deleteFileIfAny (contact, "hidden");
+    }
+
+    // delete the exchange and hidden files, if any
+    // but only if the exchange is really complete,
+    // otherwise don't delete, and force the user to click the "cancel"
+    // button (which deletes the entire contact) next time we restart
+    public static void completeExchange(String contact) {
+        if (fileExists (contact, "contact_pubkey")) {
+            deleteFileIfAny (contact, "hidden");
+            deleteFileIfAny (contact, "exchange");
+        }
+    }
+
+    public static keyExchangeComplete contactComplete(String contactName) {
+        if (! fileExists (contactName, "contact_pubkey")) {
+            return keyExchangeComplete.INCOMPLETE_NO_KEY;
+        } else if (fileExists (contactName, "exchange")) {
+            return keyExchangeComplete.INCOMPLETE_WITH_KEY;
+        } else {
+            return keyExchangeComplete.COMPLETE;
+        }
+    }
+
+    public static String contactExchangeFile(String contactName) {
+        return fileContents (contactName, "exchange");
+    }
+
+    private static String [] splitExchangeFile(String contactName) {
+        String contents = fileContents (contactName, "exchange");
+        if (contents == null)
+            return null;
+        return contents.split ("\n");
+    }
+
+    // return 0 if no exchange file, else 1 or (typically) 6 for hop count
+    public static int exchangeHopCount(String contactName) {
+        String [] contents = splitExchangeFile(contactName);
+        if ((contents == null) || (contents.length < 1)) {
+            return 0;
+        }
+        try {
+            return Integer.parseInt (contents [0]);
+        } catch (java.lang.NumberFormatException e) {
+            return 0;
+        }
+    }
+
+    // return null if no exchange file or no first secret
+    public static String firstSecret(String contactName) {
+        String [] contents = splitExchangeFile(contactName);
+        if ((contents == null) || (contents.length < 2)) {
+            return null;
+        }
+        return contents [1];
+    }
+
+    // return null if no exchange file or no second secret
+    public static String secondSecret(String contactName) {
+        String [] contents = splitExchangeFile(contactName);
+        if ((contents == null) || (contents.length < 3)) {
+            return null;
+        }
+        return contents [2];
     }
 
 };
