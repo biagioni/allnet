512a0d7e4d1cee4e8e3fa4a31455c2fb05aadfbf
edo <esb@hawaii.edu>
2018-04-25 15:49:33 -1000

    pid_bloom.c: improved (fixed?) pid_is_in_bloom and the unit test code

--- src/lib/pid_bloom.c
+++ src/lib/pid_bloom.c
@@ -3,7 +3,7 @@
 
 /* command to compile it as a stand-alone program for testing
    of the caches:
-   gcc -Wall -g -o pid_bloom_test -DTEST_PID_CACHE src/lib/pid_bloom.c src/lib/util.c src/lib/pipemsg.c src/lib/sha.c  src/lib/allnet_queue.c src/lib/allnet_log.c src/lib/ai.c src/lib/configfiles.c -lpthread
+   gcc -Wall -g -o pid_bloom_test -DTEST_PID_BLOOM src/lib/pid_bloom.c src/lib/util.c src/lib/pipemsg.c src/lib/sha.c  src/lib/allnet_queue.c src/lib/allnet_log.c src/lib/ai.c src/lib/configfiles.c -lpthread
 */
 
 #include <stdio.h>
@@ -21,7 +21,7 @@
 #define FILTER_WIDTH	65536   /* 16 bits at a time */
 #define FILTER_BITS	16      /* 16 bits at a time */
 
-/* bloom filter size should be 2MB = 2 * 16 * 8 * 65536 / 8 */
+/* bloom filter size should be 4MB = 4 * 16 * 8 * 65536 / 8 */
 
 static char bloom_filter [PID_FILTER_SELECTORS] [NUM_FILTERS]
                          [FILTER_DEPTH] [FILTER_WIDTH / 8];
@@ -54,8 +54,9 @@ static void init_bloom ()
   }
 }
 
-/* filter_selector should be 1 for acks, 0 for messages */
-/* return 1 if the id (of size FILTER_BITS/8 * FILTER_DEPTH) is found
+/* id should refer to at least 16 bytes, and PID_SIZE should be 16 or more
+ * filter_selector should be one of the PID_*_FILTER values
+ * return 1 if the id (of size FILTER_BITS/8 * FILTER_DEPTH) is found
  * in one of the filters. */
 int pid_is_in_bloom (const char * id, int filter_selector)
 {
@@ -71,20 +72,27 @@ int pid_is_in_bloom (const char * id, int filter_selector)
   init_bloom ();
   int filter_num;
   for (filter_num = 0; filter_num < NUM_FILTERS; filter_num++) {
+    int found = 1;
     int filter_depth;
     for (filter_depth = 0; filter_depth < FILTER_DEPTH; filter_depth++) {
-      uint16_t pos = readb16 (id + filter_depth * 2);
+      uint16_t pos = readb16 ((char *) id + filter_depth * 2);
       assert (pos < FILTER_WIDTH);
       uint16_t index = pos / 8;
       uint16_t offset = pos % 8;
       int byte =
         bloom_filter [filter_selector] [filter_num] [filter_depth] [index];
       int bit = byte & (1 << offset);
-      if (bit == 0)
-        return 0;
+      if (bit == 0) {  /* the ID is not in this filter */
+        found = 0;
+        break;
+      }
     }
+    if (found && (filter_num > 0))  /* also add into the top-level filter */
+      pid_add_to_bloom (id, filter_selector);
+    if (found)
+      return 1;
   }
-  return 1;
+  return 0;
 }
 
 /* add this id/ack to filter 0 */
@@ -102,7 +110,7 @@ void pid_add_to_bloom (const char * id, int filter_selector)
   init_bloom ();
   int filter_depth;
   for (filter_depth = 0; filter_depth < FILTER_DEPTH; filter_depth++) {
-    uint16_t pos = readb16 (id + filter_depth * 2);
+    uint16_t pos = readb16 ((char *) id + filter_depth * 2);
     assert (pos < FILTER_WIDTH);
     uint16_t index = pos / 8;
     uint16_t offset = pos % 8;
@@ -133,14 +141,19 @@ void pid_advance_bloom ()
   init_bloom ();
   int sel;
   for (sel = 0; sel < PID_FILTER_SELECTORS; sel++) {
-    int index = 0;
-    for (index = 0; index + 1< NUM_FILTERS; index++)
+    int index;
+/* for (index = NUM_FILTERS - 1; index >= 0; index--)
+printf ("memcpy (%p, %p, %zd) (%p...%p)\n", &(bloom_filter [sel] [index + 1]),
+&(bloom_filter [sel] [index]), sizeof (bloom_filter [sel] [index]),
+bloom_filter, ((char *) bloom_filter) + sizeof (bloom_filter)); */
+    for (index = NUM_FILTERS - 2; index >= 0; index--)
       memcpy (&(bloom_filter [sel] [index + 1]), &(bloom_filter [sel] [index]),
               sizeof (bloom_filter [sel] [index]));
+    memset (&(bloom_filter [sel] [0]), 0, sizeof (bloom_filter [sel] [0]));
   }
 }
 
-#ifdef TEST_PID_CACHE
+#ifdef TEST_PID_BLOOM
 
 static void print_sparse_buffer (const char * buffer, int bsize,
                                  const char * desc)
@@ -154,22 +167,42 @@ static void print_sparse_buffer (const char * buffer, int bsize,
 
 int main ()
 {
-  char id [FILTER_BITS/8 * FILTER_DEPTH];
-  assert (sizeof (id) == 16);
-  random_bytes (id, sizeof (id));
-  print_buffer (id, 16, "random id    ", 16, 1);
-  if (pid_is_in_bloom (id, 0))
-    print_buffer (id, 16, "id is in bloom before adding\n", 16, 1);
-  pid_add_to_bloom (id, 0);
-  id [1] |= 0x2;
-  id [3] |= 0x4;
-  id [5] |= 0x8;
-  id [7] |= 0x10;
-  print_buffer (id, 16, "new random id", 16, 1);
-  if (pid_is_in_bloom (id, 0))
-    print_buffer (id, 16, "second id is in bloom before adding\n", 16, 1);
-  pid_add_to_bloom (id, 0);
-  print_sparse_buffer ((char *) bloom_filter, BLOOM_SIZE, "filter");
+  char id1 [MESSAGE_ID_SIZE];
+  assert (sizeof (id1) == 16);
+  assert (sizeof (id1) == FILTER_BITS/8 * FILTER_DEPTH);
+  random_bytes (id1, sizeof (id1));
+  print_buffer (id1, 16, "random id1    ", 16, 1);
+  if (pid_is_in_bloom (id1, 0))
+    print_buffer (id1, 16, "id1 is in bloom before adding\n", 16, 1);
+  pid_add_to_bloom (id1, 0);
+  print_sparse_buffer ((char *)bloom_filter, BLOOM_SIZE, "filter");
+  if (! pid_is_in_bloom (id1, 0))
+    print_buffer (id1, 16, "id1 not in bloom after adding\n", 16, 1);
+  pid_advance_bloom ();
+  print_sparse_buffer ((char *)bloom_filter, BLOOM_SIZE, "advanced");
+  if (! pid_is_in_bloom (id1, 0))
+    print_buffer (id1, 16, "id1 not in bloom after advance\n", 16, 1);
+  print_sparse_buffer ((char *)bloom_filter, BLOOM_SIZE, "queried");
+  char id2 [MESSAGE_ID_SIZE];
+  memcpy (id2, id1, sizeof (id2));
+  id2 [0] ^= 0x10;
+  id2 [5] ^= 0x08;
+  print_buffer (id2, 16, "new random id", 16, 1);
+  if (pid_is_in_bloom (id2, 0))
+    print_buffer (id2, 16, "id2 is in bloom before adding\n", 16, 1);
+  pid_advance_bloom ();
+  if (! pid_is_in_bloom (id1, 0))
+    print_buffer (id1, 16, "id1 not in bloom after advance2\n", 16, 1);
+  pid_add_to_bloom (id2, 0);
+  if (! pid_is_in_bloom (id2, 0))
+    print_buffer (id2, 16, "id2 not in bloom after adding\n", 16, 1);
+  pid_advance_bloom ();
+  pid_advance_bloom ();
+  pid_advance_bloom ();
+  if (! pid_is_in_bloom (id1, 0))
+    print_buffer (id1, 16, "id1 not in bloom after advance3\n", 16, 1);
+  if (! pid_is_in_bloom (id2, 0))
+    print_buffer (id2, 16, "id2 not in bloom after advance\n", 16, 1);
 }
 
-#endif /* TEST_PID_CACHE */
+#endif /* TEST_PID_BLOOM */
