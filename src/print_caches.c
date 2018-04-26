845916178ed44ec40e952162556def2513c3fff1
edo <esb@hawaii.edu>
2018-04-25 16:50:50 -1000

    print_caches.c: reverted to 'not implemented' for now

--- src/print_caches.c
+++ src/print_caches.c
@@ -1,9 +1,14 @@
 /* print the contents of the caches */
 
+#include <stdio.h>
+
 int main (int argc, char ** argv)
 {
+  printf ("print_caches not implemented\n");
+#if 0
   extern void print_caches (int print_msgs, int print_acks);
 
   print_caches (5, 1);
+#endif /* 0 */
   return 0;
 }
